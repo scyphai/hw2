bae96eb2c8f650f3b154cd2d08d4cb1f70b647f1
Everett <scyphai@gmail.com>
2019-11-16 12:18:57 -0800

    bash and python complete

---
+++ hw2shell.sh
@@ -0,0 +1,7 @@
+cat templates/top.html content/about.html templates/bottom.html > docs/about.html
+
+cat templates/top.html content/blog.html templates/bottom.html > docs/blog.html
+
+cat templates/top.html content/contact.html templates/bottom.html > docs/contact.html
+
+cat templates/top.html content/index.html templates/bottom.html > docs/index.html
